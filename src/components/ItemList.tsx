9f1a3fc45e8a30ceba067e4b5bbfc3a132b35cc0
Saeka03 <143541044+Saeka03@users.noreply.github.com>
2024-03-23 03:51:26 +0900

    create items (#1)

---
+++ src/components/ItemList.tsx
@@ -0,0 +1,20 @@
+export const ItemList = () => {
+  return (
+    <div className="item-list">
+      <ul>
+        <li>
+          <h3>Coffee</h3>
+          <button>Add</button>
+        </li>
+        <li>
+          <h3>Tea</h3>
+          <button>Add</button>
+        </li>
+        <li>
+          <h3>Cake</h3>
+          <button>Add</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
