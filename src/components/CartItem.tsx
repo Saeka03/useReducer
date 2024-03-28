ee0b3705f407f230b4fd5996deafc857c37eb503
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-03-29 01:47:15 +0900

    create the delete handler

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,13 +1,18 @@
+import { useCartContext } from "../contexts/CartContext";
 import { Item } from "../helper/types";
 
-const CartItem = ({ title, image, quantity, totalPrice }: Item) => {
+const CartItem = ({ id, title, image, price, quantity, totalPrice }: Item) => {
+  const { deleteItemHandler } = useCartContext();
+  const deleteHandler = () => {
+    deleteItemHandler(id, price, quantity);
+  };
   return (
     <li>
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <h4>Quantity: {quantity}</h4>
       <h4>Total Price: ${totalPrice}</h4>
-      <button>Delete</button>
+      <button onClick={deleteHandler}>Delete</button>
     </li>
   );
 };
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,14 +1,22 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import {
+  ReactNode,
+  Reducer,
+  createContext,
+  useContext,
+  useReducer,
+} from "react";
 import { Item } from "../helper/types";
 
 type CartContextType = {
   items: Item[];
   addItemHandler: (addedItem: Item) => void;
+  deleteItemHandler: (itemId: number, quantity: number, price: number) => void;
 };
 
 const INITIAL_CONTEXT: CartContextType = {
   items: [],
   addItemHandler: () => {},
+  deleteItemHandler: () => {},
 };
 
 export const CartItemContext = createContext<CartContextType>(INITIAL_CONTEXT);
@@ -17,23 +25,72 @@ type childrenProps = {
   children: ReactNode;
 };
 
+type ItemAction =
+  | {
+      type: "add";
+      addedItem: Item;
+    }
+  | { type: "delete"; itemId: number; quantity: number; price: number };
+
+type ItemReducer = Reducer<Item[], ItemAction>;
+
+const INITIAL_STATE: Item[] = [];
+
 export const CartContextProvider = ({ children }: childrenProps) => {
-  const [cartItemList, setCartItem] = useState<Item[]>([]);
+  const cartItemReducer: ItemReducer = (prevState, action) => {
+    switch (action.type) {
+      case "add":
+        return [...prevState, action.addedItem];
+
+      case "delete":
+        return prevState.filter((item) => item.id !== action.itemId);
+      default:
+        break;
+    }
+    return prevState;
+  };
+
+  const [cartItemList2, dispatch] = useReducer<ItemReducer>(
+    cartItemReducer,
+    INITIAL_STATE
+  );
+
   const addItemHandler = (addedItem: Item) => {
-    const existingItemIndex = cartItemList.findIndex(
+    const existingItemIndex = cartItemList2.findIndex(
       (item) => item.id === addedItem.id
     );
     if (existingItemIndex !== -1) {
-      const newCartItemList = [...cartItemList];
+      const newCartItemList = [...cartItemList2];
       newCartItemList[existingItemIndex].quantity += 1;
       newCartItemList[existingItemIndex].totalPrice += addedItem.price;
     } else {
-      setCartItem((prevState) => [...prevState, addedItem]);
+      dispatch({ type: "add", addedItem });
+    }
+  };
+
+  const deleteItemHandler = (
+    itemId: number,
+    price: number,
+    quantity: number
+  ) => {
+    if (quantity > 1) {
+      const existingItemIndex = cartItemList2.findIndex(
+        (item) => item.id === itemId
+      );
+      if (existingItemIndex !== -1) {
+        const newCartItemList = [...cartItemList2];
+        newCartItemList[existingItemIndex].quantity -= 1;
+        newCartItemList[existingItemIndex].totalPrice -= price;
+      }
+    } else {
+      dispatch({ type: "delete", itemId, quantity, price });
     }
   };
 
   return (
-    <CartItemContext.Provider value={{ items: cartItemList, addItemHandler }}>
+    <CartItemContext.Provider
+      value={{ items: cartItemList2, addItemHandler, deleteItemHandler }}
+    >
       {children}
     </CartItemContext.Provider>
   );
